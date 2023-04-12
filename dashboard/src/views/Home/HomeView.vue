6775502248a484987d4da21aa861e7f8767f75e3
gabrielcalheiros <gabrielcalheiros@uneb.br>
2023-04-12 09:53:34 -0300

    feat(app): Created the Home, Contact and Custom View

--- dashboard/src/views/Home/HomeView.vue
+++ dashboard/src/views/Home/HomeView.vue
@@ -1,7 +1,40 @@
 <template>
-  <h1 class="font-black text-brand-main">TELA HOME</h1>
+  <!-- Vue3 já permite ter +1 elemento no root/template da aplicação -->
+  <custom-header @create-account="handleAccountCreate" @login="handleLogin" />
+  <contact-view />
+  <div class="flex justify-center py-10 bg-brand-gray">
+    <p class="font-medium text-center text-gray-800">Feedbacker © 2023</p>
+  </div>
 </template>
 
 <script>
-export default {}
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import CustomHeader from './CustomHeader.vue'
+import ContactView from './ContactView.vue'
+
+export default {
+  components: { CustomHeader, ContactView },
+  setup () {
+    // No Vue2 utilizava-se o router (this.$router.metodos). Na composition API não existe this, então foi usado Hooks.
+    // Foi criado a constante "router" para isso, usando o useRouter que está dentro do próprio Vue Router.
+    const router = useRouter()
+
+    onMounted(() => { // Quando o componente for montado...
+      const token = window.localStorage.getItem('token')
+
+      if (token) {
+        router.push({ name: 'Feedbacks' })
+      }
+    })
+
+    function handleLogin () {
+    }
+
+    function handleAccountCreate () {
+    }
+
+    return { handleLogin, handleAccountCreate }
+  }
+}
 </script>
